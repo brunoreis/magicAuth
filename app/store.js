df1ca7edfd6e6f3f1562bca628debe625df6113a
Bruno Reis <bruno.p.reis@gmail.com>
2022-02-14 01:24:22 -0300

    persist into localstorage

--- app/store.js
+++ app/store.js
@@ -1,26 +1,53 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+
+
 import authenticationReducer, { getIssuer } from '../features/authentication/authenticationSlice';
 import usersReducer, { receiveUsername } from '../features/users/usersSlice';
 import appReducer from '../features/app/appSlice';
+
 import createSagaMiddleware from 'redux-saga';
 import sagas from './sagas';
+
+const rootReducer = combineReducers({
+  authentication: authenticationReducer,
+  users: usersReducer,
+  app: appReducer,
+})
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const createStore = () => {
   const sagaMiddleware = createSagaMiddleware();
   const store = configureStore({
-    reducer: {
-      authentication: authenticationReducer,
-      users: usersReducer,
-      app: appReducer,
-    },
+    reducer: persistedReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(sagaMiddleware),
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(sagaMiddleware),
   });
   sagaMiddleware.run(sagas);
   return store
 }
 export const store = createStore()
-
+let persistor = persistStore(store)
 
 
 // thunks 
